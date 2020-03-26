987f75528d79cbfab59588d982f52958005be4de
gonzn151 <nicolas.g@globant.com>
2020-03-26 12:30:33 -0500

    Add bottom bar nav & create items for navigation

--- app/src/main/java/com/example/covidtracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/ui/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.covidtracker.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.Window
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import com.example.covidtracker.R
 import com.example.covidtracker.extensions.selectTab
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +21,16 @@ class MainActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
 
-        initHome()
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> {
+                    initHome()
+                }
+                R.id.action_search -> {}
+                R.id.action_worldwide -> {}
+            }
+            true
+        }
     }
 
     private fun initHome() {
