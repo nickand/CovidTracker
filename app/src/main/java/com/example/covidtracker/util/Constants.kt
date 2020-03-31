d974882cd011dbcd52db0f282099e66363e5f9e7
gonzn151 <nicolas.g@globant.com>
2020-03-31 01:59:13 -0500

    Remove api key

--- app/src/main/java/com/example/covidtracker/util/Constants.kt
+++ app/src/main/java/com/example/covidtracker/util/Constants.kt
@@ -1,8 +1,10 @@
 package com.example.covidtracker.util
 
+import com.example.covidtracker.BuildConfig
+
 object Constants{
 
     const val BASE_URL = "https://coronavirus-monitor.p.rapidapi.com/"
-    const val API_KEY = "618954eaeamshcf628e1bff076ddp1eae82jsn911ee197de3f"
+    const val API_KEY = BuildConfig.API_KEY
     const val MASK_INSTRUCTIONS_URL = BASE_URL + "coronavirus/masks.php"
 }
